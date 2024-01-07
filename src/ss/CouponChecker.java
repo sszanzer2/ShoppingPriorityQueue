2ccbfd1ab2af639961459d1a85b59c6116dcf30b
sszanzer2 <122063650+sszanzer2@users.noreply.github.com>
2024-01-07 12:52:09 -0500

    Update CouponChecker.java

--- src/ss/CouponChecker.java
+++ src/ss/CouponChecker.java
@@ -3,10 +3,15 @@ package ss;
 public class CouponChecker {
     public static final double COUPON_THRESHOLD = 100.0;
     public static final double COUPON_AMOUNT = 0.10;
+    
+    public boolean isCouponEligible(Shopper shopper) {
+    	// Check if the shopper is eligible for a coupon (e.g., total purchases > $100)
+        return shopper.getTotalPurchases() > COUPON_THRESHOLD;
+    }
 
-    public void applyCoupon(Shopper shopper) {
-        // Check if the shopper is eligible for a coupon (e.g., total purchases > $100)
-        if (shopper.getTotalPurchases() > COUPON_THRESHOLD) {
+    //added printing to the title to tell users it prints something
+    public void printingAndApplyCoupon(Shopper shopper) {
+    	 if (isCouponEligible(shopper)) {
             System.out.printf("%s total purchases: $%,.2f%n", shopper.getName(), shopper.getTotalPurchases());
             // Adjust the total purchases with a coupon
             shopper.updateTotalPurchases(-shopper.getTotalPurchases() * COUPON_AMOUNT);
