1ae4d36937d090a73c9fb258f39bfeee7936854e
sszanzer2 <122063650+sszanzer2@users.noreply.github.com>
2024-01-07 12:54:37 -0500

    Update StoreOperations.java

--- src/ss/StoreOperations.java
+++ src/ss/StoreOperations.java
@@ -3,6 +3,7 @@ package ss;
 public interface StoreOperations {
     void addItem(String itemName, double price, int quantity);
     boolean purchaseItem(Shopper shopper, String itemName, int quantity);
-    double getItemPrice(String itemName);
+	Object getItemQuantities();
+	double getItemPrice(String itemName);
 }
 
