9e8e6ad99c6afe095941d0fb998e0b601acfab65
sszanzer2 <122063650+sszanzer2@users.noreply.github.com>
2024-01-07 12:53:42 -0500

    Update ShoppingSystem.java

--- src/ss/ShoppingSystem.java
+++ src/ss/ShoppingSystem.java
@@ -3,9 +3,11 @@ package ss;
 // ShoppingSystem class manages shoppers using a priority queue
 public class ShoppingSystem implements ShopperCheckout {
     private PriorityQueue<Shopper> priorityQueue;
+    private CouponChecker couponChecker;
 
-    public ShoppingSystem() {
-        priorityQueue = new PriorityQueue<>();
+    public ShoppingSystem(CouponChecker couponChecker) {
+        this.priorityQueue = new PriorityQueue<>();
+        this.couponChecker = couponChecker;
     }
 
     // Add a shopper to the priority queue
@@ -17,14 +19,19 @@ public class ShoppingSystem implements ShopperCheckout {
     @Override
     public void simulateCheckout() {
         while (!priorityQueue.isEmpty()) {
-            Shopper shopper = priorityQueue.dequeue(); // Use dequeue method to get the next shopper
-            CouponChecker couponChecker=new CouponChecker();
-            // Apply the coupon after the shopper is done with purchases
-            couponChecker.applyCoupon(shopper);
-
-            System.out.printf("Checking out: %s (Insertion Order: %d) - Total Purchase Amount: $%,.2f%n",
-                    shopper.getName(), shopper.getInsertionOrder(), shopper.getTotalPurchases());
+            Shopper shopper = priorityQueue.dequeue();
+            applyCoupon(shopper);
+            printCheckoutInformation(shopper);
         }
+    }
+
+    private void applyCoupon(Shopper shopper) {
+        couponChecker.printingAndApplyCoupon(shopper);
+    }
+
+    private void printCheckoutInformation(Shopper shopper) {
+        System.out.printf("Checking out: %s (Insertion Order: %d) - Total Purchase Amount: $%,.2f%n",
+                shopper.getName(), shopper.getInsertionOrder(), shopper.getTotalPurchases());
     }
 
     @Override
